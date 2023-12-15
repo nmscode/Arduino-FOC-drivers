e3b4c66ec3c32f502239c03a7eee372648e25443
nmscode <105442557+nmscode@users.noreply.github.com>
2023-12-15 17:47:06 -0500

    demod logic fix

--- src/encoders/flux_observer/FluxObserverSensor.cpp
+++ src/encoders/flux_observer/FluxObserverSensor.cpp
@@ -99,7 +99,7 @@ void FluxObserverSensor::update() {
         //delta_i_dh=d_lp.getLp(_motor->hfi_state*(i_dh-i_dh_prev));
         
         atan_test=_atan2(i_qh-i_qh_prev,i_dh-i_dh_prev);
-        e=q_lp.getLp((i_qh-i_dh)*_cos(_normalizeAngle(micros()*_2PI/((1.0f/hfi_frequency)*1000000.0f))));//ke*delta_i_qh;
+        e=q_lp.getLp((i_qh*iq-i_dh*i_dh)*_cos(_normalizeAngle(micros()*_2PI/((1.0f/hfi_frequency)*1000000.0f))));//ke*delta_i_qh;
 
         
         //Position Observer
